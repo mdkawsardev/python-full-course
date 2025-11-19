8f1cb029bd1c7e30056de53fea9a92ed7708481c
mdkawsar <info.mdkawsardev@gmail.com>
2025-11-19 11:24:53 +0600

    dlkfjd

---
+++ 10_operators/arithmetic.py
@@ -0,0 +1,36 @@
+# Arithmetic Operators
+#? Arithmetic operators are used with numeric values to perform common mathematical operations:
+
+"""
+? (+)       Addition            x + y  
+
+? (-)       Substraction        x - y
+
+? (*)       Multiplication      x * y
+
+? (/)       Division            x / y
+
+? (%)       Modulus             x % y
+
+? (**)      Exponentiation      x ** y squre
+
+? (//)    Floor division        x // y
+
+"""
+
+#? Here is an example using different arithmetic operators:
+
+a = 5
+b = 3
+
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
+print(a % b)
+print(a ** b)
+print(a // b)
+#? / division(Returns a float)
+#? // Floor division(Always returns an int)
+print(12/5) #? Returns a float
+print(12//5) #? Returns an int
