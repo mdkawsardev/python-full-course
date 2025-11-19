020d3b3c9cdc144cd55fa0cde545fbb8406a78af
mdkawsar <info.mdkawsardev@gmail.com>
2025-11-19 22:21:57 +0600

    jdfj

---
+++ 10_operators/3_comparison.py
@@ -0,0 +1,28 @@
+# Comparison Operators
+#? Comparison operators are used to compare two values:
+
+"""
+? ==            equal                    x == y
+? !=            not equal                x != y
+? >             greater than             x > y
+? <             less than                x < y
+? >=            greater than or equal    x >= y
+? <=            less than or equal       x <= y
+
+"""
+#* Comparison operators return True or False based on the comparison:
+
+x = 5
+y = 6
+print(x == y)
+print(x != y)
+print(x > y)
+print(x < y)
+print(x >= y)
+print(x <= y)
+
+#? Chaining Comparison Operators
+#* Chaining Comparison Operators
+print(x == y and x >= y)
+print(x <= y or x != y)
+
