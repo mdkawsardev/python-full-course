dc32a8f39c385c1a1abc2bf2f6c08f5344a6ca79
mdkawsar <info.mdkawsardev@gmail.com>
2025-11-24 21:33:11 +0600

    dlfdd

---
+++ 12_tuples/6_join_tuple.py
@@ -0,0 +1,10 @@
+# Join Tuples in Python
+#? Join Two Tuples
+#* To join two or more tuples you can use the + operator:
+tuple1 = (1, 2, 3, 4, 5, 6)
+tuple2 = (7, 8, 9, True, False)
+tuple3 = tuple1 + tuple2
+print(tuple3)
+#* Multiply tuples
+tuple4 = tuple1 * 2 #* or more
+print(tuple4)
