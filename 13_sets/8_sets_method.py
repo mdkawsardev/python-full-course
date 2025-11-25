e695825e7f2ffe41097975aaea83e4927123e753
mdkawsar <info.mdkawsardev@gmail.com>
2025-11-25 22:23:46 +0600

    dfdl

---
+++ 13_sets/8_sets_method.py
@@ -0,0 +1,19 @@
+# Sets methods in Python
+
+"""                          
+?Method                       Shortcut          Description
+add()                                           Adds new items to a set
+clear()                                         Clears all items from a set
+copy()                                          Copies a new set
+difference()                    -               Keeps all items from first set that are not in other sets
+difference_update()             -=              Removes the items in this set that are also included in another
+discard()                                       Removes a specified item
+intersection()                  &               Returns all duplicates items
+intersection_update()           &=              Removes the items in this set that are not present in other
+pop()                                           Removes an element from the set
+remove()                                        Removes the specified element
+symmetric_difference()          ^               Returns all items except duplicates
+symmetric_difference_update()   ^=              Changes the main set with all unique items
+union()                         |               Combines two or more sets 
+update()                        |=              Changes the main set with specified values or new sets
+"""
