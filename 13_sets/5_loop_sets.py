e89bee69421247ddb817e7e694d48b24ee76f6ed
mdkawsar <info.mdkawsardev@gmail.com>
2025-11-25 15:09:00 +0600

    jdfldj

---
+++ 13_sets/5_loop_sets.py
@@ -0,0 +1,10 @@
+# Loop Sets
+#? Loop Items
+#* You can loop through the set items by using a for loop:
+set1 = {"January", "February", "March", "April", "May", "Jun", "July", "August", "September", "November", "December"}
+for i in set1: #* Through items
+    print(i)
+
+#! We cannot access set items using while loop. Set object is not subscriptable and it's unordered/unindexed
+
+
