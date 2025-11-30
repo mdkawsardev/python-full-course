b879ce3a4b2b32af3b3eef737de796d67424bb68
mdkawsar <info.mdkawsardev@gmail.com>
2025-11-30 15:23:35 +0600

    chapter three is completed

--- 19_functions/3_args_kwargs.py
+++ 19_functions/3_args_kwargs.py
@@ -108,3 +108,24 @@ def combining(username, *colors, **details):
     for d in details:
         print(f"{d} = {details[d]}")
 combining("mdkawsardeveloper", "black", "white", "blue", "skyblue", "gray", name="Imran Kawsar", age=23, profession="Python developer")
+
+#? Unpacking Arguments
+#* The * and ** operators can also be used when calling functions to unpack (expand) a list or dictionary into separate arguments.
+#* Unpacking Lists with *
+#* If you have values stored in a list, you can use * to unpack them into individual arguments:
+#* Using * to unpack a list into arguments:
+def newFun(a, b, c):
+    return a+b+c
+myList = [1, 2, 3]
+result = newFun(*myList) #* Same as: newFun(1, 2, 3)
+print(result)
+
+#? Unpacking Dictionaries with **
+#* If you have keyword arguments stored in a dictionary, you can use ** to unpack them:
+#* Using ** to unpack a dictionary into keyword arguments:
+def dicFun(fname, lname):
+    print("Hi", fname, lname)
+myDict = {'fname': 'imran', 'lname': 'kawsar'}
+newResult = dicFun(**myDict)
+#! Remember: Use * and ** in function definitions to collect arguments, and use them in function calls to unpack arguments.
+
