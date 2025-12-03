c271c858bca9daba728b62a8368fb5f940c3a119
mdkawsar <info.mdkawsardev@gmail.com>
2025-12-03 15:46:40 +0600

    json is done

---
+++ 22_json/json.py
@@ -0,0 +1,7 @@
+# Python JSON
+#* JSON is a syntax for storing and exchanging data.
+#* JSON is text, written with JavaScript object notation.
+
+#? JSON in Python
+#* Python has a built-in package called 'json', which can be used to work with JSON data.
+
