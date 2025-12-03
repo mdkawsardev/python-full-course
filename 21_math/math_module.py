af698783abdc27697f339d5724daf32c850d0041
mdkawsar <info.mdkawsardev@gmail.com>
2025-12-03 15:43:53 +0600

    math module is done

--- 21_math/math_module.py
+++ 21_math/math_module.py
@@ -1,3 +1,14 @@
 #? The Math Module
 #* Python has also a built-in module called 'math', which extends the list of mathematical functions.
 #* To use it, you must import the 'math' module:
+import math
+#* When you have imported the 'math' module, you can start using methods and constants of the module.
+#* The math.sqrt() method for example, returns the square root of a number:
+x = math.sqrt(64)
+print(x)
+
+#? The 'math.ceil()' method rounds a number upwards to its nearest integer, and the 'math.floor()' method rounds a number downwards to its nearest integer, and returns the result:
+y = math.ceil(2.5) #* Round a number upward to its nearest integer
+z = math.floor(2.5) #* Round a number downward to its nearest integer
+print(y)
+print(z)
